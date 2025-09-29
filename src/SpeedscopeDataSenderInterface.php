463f543761bf20efe6f0c8562c963b53494e7b00
Bozhidar Hristov <warxcell@gmail.com>
2025-09-29 21:38:41 +0300

    add sender interface

--- src/SpeedscopeDataSenderInterface.php
+++ src/SpeedscopeDataSenderInterface.php
@@ -40,47 +40,10 @@ use const JSON_THROW_ON_ERROR;
 use const JSON_UNESCAPED_SLASHES;
 use const JSON_UNESCAPED_UNICODE;
 
-final readonly class SpeedscopeDataSender implements SpeedscopeDataSenderInterface
+interface SpeedscopeDataSenderInterface
 {
-    public function __construct(
-        private string $url,
-        private ClientInterface $httpClient,
-        private RequestFactoryInterface $requestFactory,
-        private StreamFactoryInterface $streamFactory,
-    ) {
-    }
-
     /**
-     * @throws \Psr\Http\Client\ClientExceptionInterface
-     * @throws JsonException
+     * @throws \Exception
      */
-    public function __invoke(string $name, array $data, ?array $context = null): void
-    {
-        $this->httpClient->sendRequest(
-            $this->requestFactory->createRequest('POST', $this->url)
-                ->withBody(
-                    $this->streamFactory->createStream(
-                        json_encode([
-                            'name' => $name,
-                            'data' => $this->utf8ize($data),
-                            'context' => $context,
-                        ], flags: JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR)
-                    )
-                )
-                ->withHeader('Content-Type', 'application/json')
-        );
-    }
-
-    private function utf8ize(mixed $mixed): mixed
-    {
-        if (is_array($mixed)) {
-            foreach ($mixed as $key => $value) {
-                $mixed[$key] = $this->utf8ize($value);
-            }
-        } elseif (is_string($mixed)) {
-            return mb_convert_encoding($mixed, 'UTF-8', 'UTF-8');
-        }
-
-        return $mixed;
-    }
+    public function __invoke(string $name, array $data, ?array $context = null): void;
 }
